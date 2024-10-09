d15dc2eb1a30561f0b7fd58bd196bd02bfc871a6
Muhammed suhaid <muhammedsuhaid3@gmail.com>
2024-10-09 21:01:57 +0530

    CO2 - program1, program2, program3

---
+++ Python programming Lab/CO2/program2.py
@@ -0,0 +1,13 @@
+n=10
+
+num1=0
+num2=1
+
+print(num1)
+print(num2)
+
+for i in range(3,n+1):
+    num3=num1+num2
+    print(num3)
+    num1=num2
+    num2=num3
---
+++ Python programming Lab/CO2/program3.py
@@ -0,0 +1,7 @@
+lst=[1,2,3,4,5]
+sum=0
+
+for i in lst:
+    sum=sum+i
+
+print(f"Sum of all items in the list is {sum}")
